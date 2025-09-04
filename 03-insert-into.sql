8fb29ced49120eb545d207e8cc7fe07809b301dc
atrox3d <atrox3d@gmail.com>
2025-09-04 06:01:47 +0000

    03-insert-into.sql

---
+++ 03-insert-into.sql
@@ -0,0 +1,18 @@
+INSERT INTO person (
+    first_name,
+    last_name,
+    gender,
+    date_of_birth
+) VALUES (
+    'Anne', 'Smith', 'FEMALE', DATE '1988-01-09'
+);
+
+INSERT INTO person (
+    first_name,
+    last_name,
+    gender,
+    date_of_birth,
+    email
+) VALUES (
+    'Jake', 'Jones', 'MALE', DATE '1990-12-31', 'jake@gmail.com'
+);
