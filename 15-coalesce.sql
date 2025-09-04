30b6c91f8a6bc935b2d48e422f1f5474ba175981
atrox3d <atrox3d@gmail.com>
2025-09-04 13:27:04 +0000

    15-coalesce.sql

---
+++ 15-coalesce.sql
@@ -0,0 +1,19 @@
+\c test
+
+-- returns the first non-null parameter
+SELECT COALESCE(1);
+
+SELECT COALESCE(null, 1) as number;
+SELECT COALESCE(null, null, 1) as number;
+SELECT COALESCE(null, null, null, 1) as number;
+
+SELECT COALESCE(null, null, null, 1, 10) as number;
+
+
+SELECT  COALESCE(email, 'EMAIL_NOT_PROVIDED') as email
+FROM    person;
+
+SELECT  COALESCE(email, 'EMAIL_NOT_PROVIDED') as email
+FROM    person
+WHERE   email IS null       -- cannot use the coalesce value EMAIL_NOT_PROVIDED, must use null
+;
