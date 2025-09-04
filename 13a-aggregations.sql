a4d7b16c315ddadf9a7210ffbd9fd16dcf42b54b
atrox3d <atrox3d@gmail.com>
2025-09-04 12:52:35 +0000

    aggregations

---
+++ 13a-aggregations.sql
@@ -0,0 +1,15 @@
+\c test
+
+
+SELECT make, model, MIN(price), MAX(price)
+FROM car
+GROUP BY make, model
+ORDER BY make, model
+;
+
+SELECT make, MIN(price), MAX(price), AVG(price), MAX(price) - MIN(price) as range
+FROM car
+GROUP BY make
+ORDER BY MAX(price) - MIN(price) DESC
+;
+
