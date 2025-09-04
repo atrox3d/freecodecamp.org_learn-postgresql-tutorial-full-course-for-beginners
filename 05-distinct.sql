2917db6348a5d13ce6d8ebd5983ee8ba820cc2c4
atrox3d <atrox3d@gmail.com>
2025-09-04 06:44:18 +0000

    05-distinct.sql

---
+++ 05-distinct.sql
@@ -0,0 +1,14 @@
+\c test
+
+select country_of_birth 
+FROM person 
+ORDER BY country_of_birth;
+
+select distinct country_of_birth 
+FROM person 
+ORDER BY country_of_birth;
+
+select 
+    count(distinct country_of_birth) as count_distinct_country,     -- 122 unique countries
+    count(*) as count_all                                           -- 1000
+FROM person;
