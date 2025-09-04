4879be37b07c5f82ab2b1fe0701decbf8ba39953
atrox3d <atrox3d@gmail.com>
2025-09-04 08:19:52 +0000

    08-in.sql

---
+++ 08-in.sql
@@ -0,0 +1,34 @@
+\c test
+
+SELECT *
+FROM person
+WHERE
+    country_of_birth = 'France'
+OR  country_of_birth = 'Germany'
+OR  country_of_birth = 'Italy'
+;
+
+SELECT *
+FROM person
+WHERE
+    country_of_birth IN ('France', 'Germany', 'Italy')  -- same as the previous
+;
+
+-- create auxiliary table to use as IN parameter
+DROP TABLE countries;
+CREATE TABLE countries (
+    country VARCHAR(50)
+);
+INSERT INTO countries 
+    (country) 
+VALUES 
+    ('France'), ('Germany'), ('Italy')
+;
+
+SELECT *
+FROM person
+WHERE
+    country_of_birth IN (
+        SELECT * from countries         -- same as the previous
+    )  
+;
