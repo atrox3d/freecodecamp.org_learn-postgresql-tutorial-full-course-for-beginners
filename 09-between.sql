dbb5c175e39fbf1ecaf1337b7778ece514dfed83
atrox3d <atrox3d@gmail.com>
2025-09-04 08:51:41 +0000

    09-between.sql

---
+++ 09-between.sql
@@ -0,0 +1,19 @@
+\c test
+
+SELECT *
+FROM person
+WHERE
+    date_of_birth BETWEEN DATE '2024-09-01' AND DATE '2024-09-15'
+;
+
+SELECT *
+FROM person
+WHERE
+    country_of_birth BETWEEN 'France' AND 'Italy'                   -- lexycographical
+;
+
+SELECT *
+FROM person
+WHERE
+    country_of_birth BETWEEN 'Italy' AND 'France'                   -- lexycographical: no records
+;
