6b4900aa4dccb2b782688c7a4c9e0425837a62a9
atrox3d <atrox3d@gmail.com>
2025-09-05 08:28:32 +0000

    add comments to connect.sh

--- connect.sh
+++ connect.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+# this uses ~/.pgpass file to infer db and password
+# host is postgres
+# user is postgres
+# script passes any additional arguments to psql
+# script can read stdin: e.g. ./connect.sh < script.sql
 psql -h postgres -U postgres ${@}
